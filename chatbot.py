9bafa738659cbbd95075b1f3ea83eecc40161823
HarshilShah1804 <143382356+HarshilShah1804@users.noreply.github.com>
2024-07-16 18:29:59 +0530

    Add comments

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+# Import the dependencies
 from llama_index.core import ServiceContext
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
 from llama_index.embeddings.gradient import GradientEmbedding
@@ -6,11 +7,13 @@ from llama_index.core.memory import ChatMemoryBuffer
 from llama_parse import LlamaParse
 import os
 
-
+# Set up the environment varianbles
 os.environ['GRADIENT_ACCESS_TOKEN'] = "ITCAjsv0jUN01CT0TQ6bFcdeYKduw01e"
 os.environ['GRADIENT_WORKSPACE_ID'] = "89d595bc-bba8-4ea5-9bd8-d5ecbf224947_workspace"
 
+# Create PDF-Chatbot class
 class PDF_Chatbot:
+    # Initial Setup
     def __init__(self):
         self.document=None
         self.llm = GradientBaseModelLLM(
@@ -30,10 +33,11 @@ class PDF_Chatbot:
         self.chat_engine = None
         self.memory = None
         self.index = None  
-        
+
+    # Function to parse the uploaded pdf using LlamaParse
     def parse(self,name):
         parser = LlamaParse(
-            api_key="llx-sCCsgFLBIwEUSbDXuq9TMZT8fZV5P12xpgjdDN3CBTekFxr5",  
+            api_key="llx-sCCsgFLBIwEUSbDXuq9TMZT8fZV5P12xpgjdDN3CBTekFxr5",  # Llama Index API key
             result_type="markdown",  
             verbose=True
         )
@@ -44,13 +48,14 @@ class PDF_Chatbot:
         self.index = VectorStoreIndex.from_documents(self.document, service_context=self.service_context)
         self.chat_engine = self.index.as_chat_engine(
             chat_mode="context",
-            memory=self.memory,
-            system_prompt=(
-                "You are formal chatbot. Give your answer in html rendering, without <html> and <body> tags. Only answer if it is present in the pdf."
+            memory=self.memory,  # Add memory of context to the model
+            system_prompt=( 
+                "You are formal chatbot. Give your answer in html rendering, without <html> and <body> tags. Only answer if it is present in the pdf."  #Initial Prompt given to the model
             ),
         )
-    
+
+    # Function to respond the query of the user
     def query(self, query):
         response = self.chat_engine.chat(query)
-        print(response)
+        # print(response)
         return response
